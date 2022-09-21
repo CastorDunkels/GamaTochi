8a50bc45aa6abc0dabefec485825c052aa08435a
Castor Dunkels <castordnk@gmail.com>
2022-09-21 14:57:42 +0200

    :I

--- GamaTochi/Program.cs
+++ GamaTochi/Program.cs
@@ -1,2 +1,51 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcum to GamaTochi! Your new beast friend.");
+
+Stats S = new Stats();
+
+Console.WriteLine("Choose a new name for your new friend:");
+
+S.Name = Console.ReadLine();
+S.Name = S.Name.ToLower();
+if (S.Name == "todd" || S.Name == "")
+{
+    S.Name = "Todd";
+    Console.WriteLine($"{S.Name} is happy that you choose to leave its name as it was.");
+
+}
+else
+{
+    Console.WriteLine($"{S.Name} will not be happy with its new name.");
+
+}
+Console.ReadKey();
+
+while(S.GetAlive() == true){
+
+    Console.Clear();
+    S.Tick();
+    S.PrintStats();
+    Console.WriteLine($"What do you want to do today? \nGive {S.Name} food: Type 1");
+    Console.WriteLine($"Talk with {S.Name}: Type 2");
+    Console.WriteLine($"Teach {S.Name} a new word: Type 3");
+    string Action = Console.ReadLine();
+    if(Action == "1"){
+        S.Feed();
+    }
+    if(Action == "2"){
+        S.Hi();
+    }
+    if(Action == "3"){
+        Console.WriteLine("What word?");
+        string word = Console.ReadLine();
+        S.Teach(word);
+    }
+    else{
+        Console.WriteLine("You chose to do nothing");
+    }
+
+
+}
+
+Console.WriteLine($"This is so sad you let {S.Name} die!");
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
--- GamaTochi/Stats.cs
+++ GamaTochi/Stats.cs
@@ -1,10 +1,69 @@
+using System;
 public class Stats
 {
-    public int Hunger = 0;
+    private int Hunger = 0;
 
-    public int Boredom = 0;
+    private int Boredom = 0;
 
-    List<string> Words = new List<string>();
+    private List<string> Words = new();
+
+    private bool isAlive = true;
+
+    private Random generator = new(); 
+
+    public string Name;
+
+    public void Feed(){
+        int rndFeed = generator.Next(3);
+        Hunger -= rndFeed;
+        if (Hunger <= 0){
+            Hunger = 0;
+        }
+    }
+
+    public void Hi(){
+        int wordNum = generator.Next(Words.Count);
+        Console.WriteLine($" {Name} says: {Words[wordNum]}");
+        ReduceBoredom();
+    }
+
+    public void Teach(string word){
+        if(!word.Contains(word)){
+            Console.WriteLine($" {Name} learns: {word}");
+            Words.Add(word);
+            ReduceBoredom();
+        }
+        else{
+            Console.WriteLine($"{Name} already knew that word");
+        }
+    }
+
+    public void Tick(){
+        Hunger ++;
+        Boredom ++;
+        if(Hunger >= 10 || Boredom >= 10){
+            isAlive = false;
+        }
+
+    }
+
+    public void PrintStats(){
+        Console.WriteLine($"Name: {Name} || Hunger: {Hunger} || Boredom: {Boredom} || Vocabulary: {Words.Count} words");
+    }
+
+    public bool GetAlive(){
+        return isAlive;
+    }
+
+    private void ReduceBoredom(){
+        int rndBoredom = generator.Next(4);
+        Boredom -= rndBoredom;
+        if (Boredom < 0){
+            Boredom = 0;
+        }
+
+    
+    }
 
 
 }
